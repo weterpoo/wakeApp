4c9737ddb7f21cd4c86c93c4d3a36881ebf0f5ce
Shotaro Ikeda <ikeda.shot@gmail.com>
2015-03-21 11:43:25 -0500

    Added facebook auth button

--- AndroidStudioProjects/MyApplication/app/src/main/java/com/example/peterwoo/myapplication/MainActivity.java
+++ AndroidStudioProjects/MyApplication/app/src/main/java/com/example/peterwoo/myapplication/MainActivity.java
@@ -1,17 +1,41 @@
 package com.example.peterwoo.myapplication;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.facebook.Session;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            // Add the fragment on initial activity setup
+            mainFragment = new MainFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(android.R.id.content, mainFragment)
+                    .commit();
+        } else {
+            // Or set the fragment from restored state info
+            mainFragment = (MainFragment) getSupportFragmentManager()
+                    .findFragmentById(android.R.id.content);
+        }
+
+        //Check if the user gave app necessary permisions
+        $permissions = $facebook->api("/me/permissions");
+        if( array_key_exists('publish_stream', $permissions['data'][0]) ) {
+            // Permission is granted!
+            ;
+        } else {
+            // We don't have the permission
+            // Alert the user or ask for the permission!
+            header( "Location: " . $facebook->getLoginUrl(array("scope" => "publish_stream")) );
+        }
     }
 
 
@@ -35,5 +59,18 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+}
+
+class MainFragment extends Fragment {
+    private MainFragment mainFragment;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.main, container, false);
+
+        return view;
     }
 }
